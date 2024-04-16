770188dd637290a35c7fbcf25cf2f7c292f9bcee
Pingquan2024 <esrgfu@gmail.com>
2024-04-16 23:15:34 +0800

    2024年4月16日初始vector，写题c++杨辉三角

--- Code2024/leetcode_exercises/workbook.cpp
+++ Code2024/leetcode_exercises/workbook.cpp
@@ -1,6 +1,8 @@
 //代码段集合
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 //字符串相加
@@ -100,5 +102,33 @@ public:
             --end;
         }
         return s;
+    }
+};
+
+
+//118.杨辉三角
+class Solution
+{
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> vv;
+        vv.resize(numRows, vector<int>());
+        for (size_t i = 0; i < vv.size(); i++)
+        {
+            vv[i].resize(i + 1, 0);
+            vv[i][0] = vv[i][vv[i].size() - 1] = 1;
+        }
+
+        for (size_t i = 0; i < vv.size(); i++)
+        {
+            for (size_t j = 0; j < vv[i].size(); j++)
+            {
+                if (vv[i][j] == 0)
+                {
+                    vv[i][j] = vv[i - 1][j] + vv[i - 1][j - 1];
+                }
+            }
+        }
+        return vv;
     }
 };
---
+++ Code2024/test_4_16/test.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+#include<time.h>
+
+void test_vector1()
+{
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+
+	for (size_t i = 0; i < v.size(); ++i)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+
+	vector<int>::iterator it = v.begin();
+	while (it != v.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+
+	for (auto e : v)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	vector<int> copy(v);
+
+	for (auto e : copy)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+}
+
+void test_vector2()
+{
+	vector<int> v1(10, 1);
+	for (auto e : v1)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	vector<int> v2(v1.begin(), v1.end());
+	for (auto e : v2)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	string s1("hello world");
+	vector<char> v3(s1.begin() + 3, --s1.end());
+	for (auto e : v3)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+}
+
+void test_vector3()
+{
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+
+	vector<int>::iterator it = v.begin();
+	while (it != v.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+
+	//vector<int>::reverse_iterator rit = v.rbegin();
+	auto rit = v.rbegin();
+	while (rit != v.rend())
+	{
+		cout << *rit << " ";
+		++rit;
+	}
+	cout << endl;
+	//cout << v.max_size() << endl;
+}
+
+void TestVectorExpand()
+{
+	size_t sz;
+	vector<int> v;
+	const size_t n = 10000000;
+
+	size_t begin = clock();
+	//v.reserve(n);
+	sz = v.capacity();
+	cout << "making v grow:\n";
+	for (int i = 0; i < n; ++i)
+	{
+		v.push_back(i);
+		if (sz != v.capacity())
+		{
+			sz = v.capacity();
+			//cout << "capacity changed: " << sz << '\n';
+		}
+	}
+	size_t end = clock();
+	cout << "消耗：" << end - begin << endl;
+}
+
+int main()
+{
+	test_vector2();
+
+	//TestVectorExpand();
+
+	return 0;
+}
