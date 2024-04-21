89556d0cbfeee7d4b5d16bcdc6b33a73ee74402b
Pingquan2024 <esrgfu@gmail.com>
2024-04-21 23:15:48 +0800

    2024年4月21日模拟Date类

--- Code2024/Date/Date.h
+++ Code2024/Date/Date.h
@@ -11,26 +11,28 @@ public:
 	//构造函数
 	Date(int year = 1970, int month = 1, int day = 1);
 
-	int GetMonthDay(int year, int month);
+	int GetMonthDay(int year, int month)const;
 
-	void Print();
+	//打印函数
+	void Print()const;
 
 	//运算符重载
-	bool operator==(const Date& d);
-	bool operator!=(const Date& d);
-	bool operator<(const Date& d);
-	bool operator>(const Date& d);
-	bool operator<=(const Date& d);
-	bool operator>=(const Date& d);
+	bool operator==(const Date& d)const;
+	bool operator!=(const Date& d)const;
+	bool operator<(const Date& d)const;
+	bool operator>(const Date& d)const;
+	bool operator<=(const Date& d)const;
+	bool operator>=(const Date& d)const;
 
 	Date& operator+=(int day);
+	Date& operator-=(int day);
 
-	Date operator+(int day);
+	Date operator+(int day)const;
 
-	//d1-100
-	Date operator-(int day);
-	//d1-d2
-	int operator-(const Date& d);
+	//日期-天数 d1-100
+	Date operator-(int day)const;
+	//日期-日期 d1-d2
+	int operator-(const Date& d)const;
 
 	// ++d1
 	Date& operator++();
@@ -38,6 +40,11 @@ public:
 	// d1++
 	Date operator++(int);     //int占位，跟前置重载区分
 
+	//前置--
+	Date& operator--();
+
+	//后置--
+	Date operator--(int);
 private:
 	int _year;
 	int _month;
