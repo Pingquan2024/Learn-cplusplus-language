67a2a9ec7b9f102e78bc88598038e8b3886a6697
Pingquan2024 <esrgfu@gmail.com>
2024-04-23 00:06:54 +0800

    leetcode，852.山脉数组的峰顶索引

--- Code2024/leetcode_exercises/workbook.cpp
+++ Code2024/leetcode_exercises/workbook.cpp
@@ -132,3 +132,23 @@ public:
         return vv;
     }
 };
+
+//852. 山脉数组的峰顶索引
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0, right = arr.size() - 1;
+
+        while (left < right)
+        {
+            int mid = (right - left + 1) / 2 + left;
+            if (arr[mid] > arr[mid - 1])
+            {
+                left = mid;
+            }
+            else right = mid - 1;
+        }
+
+        return left;
+    }
+};
