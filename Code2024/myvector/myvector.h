b398193efbec553410233352d16b92c61e1a9d6e
Pingquan2024 <esrgfu@gmail.com>
2024-04-25 23:41:13 +0800

    2024年4月25日vector模拟实现

--- Code2024/myvector/myvector.h
+++ Code2024/myvector/myvector.h
@@ -1,7 +1,33 @@
 #pragma once
+#include <iostream>
 
+//template < class T, class Alloc = allocator<T> > class vector; // generic template
 namespace me
 {
-	template<class T>
+	class myvector
+	{
+		//构造函数
+		myvector()
+		{
+
+		}
+
+		//拷贝构造
+		myvector(const myvector& v)
+		{
+
+		}
+
+		//析构函数
+		~myvector()
+		{
+
+		}
+
+	private:
+
+		int _size;
+		int _capacity;
+	};
 }
 
--- Code2024/myvector/test.cpp
+++ Code2024/myvector/test.cpp
@@ -1,2 +1,12 @@
 
 #include "myvector.h"
+
+void test_vector()
+{
+	myvector v1;
+}
+int main()
+{
+	test_vector();
+	return 0;
+}
