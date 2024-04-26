95a0a256bfaf2ba3dc4715031a1097c34e379452
Pingquan2024 <esrgfu@gmail.com>
2024-04-26 23:22:12 +0800

    vector

--- Code2024/myvector/myvector.h
+++ Code2024/myvector/myvector.h
@@ -4,10 +4,16 @@
 //template < class T, class Alloc = allocator<T> > class vector; // generic template
 namespace me
 {
+	template<class T>
 	class myvector
 	{
+	public:
+		// Vector的迭代器是一个原生指针
+		typedef T* iterator;
+
 		//构造函数
-		myvector()
+		myvector(int _size=0,int _capacity=0)
+			_size=new int[]
 		{
 
 		}
@@ -28,6 +34,10 @@ namespace me
 
 		int _size;
 		int _capacity;
+
+		iterator _start;		// 指向数据块的开始
+		iterator _finish;		// 指向有效数据的尾
+		iterator _endOfStorage;  // 指向存储容量的尾
 	};
 }
 
