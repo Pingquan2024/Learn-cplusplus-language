c3ad8238c849c54430620415dda10c1c22babc04
Pingquan2024 <esrgfu@gmail.com>
2024-05-01 23:26:04 +0800

    二叉树中序遍历

--- Code2024/leetcode_exercises/workbook.cpp
+++ Code2024/leetcode_exercises/workbook.cpp
@@ -254,3 +254,45 @@ public:
         return pPath.top();
     }
 };
+
+//二叉树中序遍历
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root)
+    {
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        vector<int> v;
+
+        while (cur || !st.empty())
+        {
+            //1.左路节点
+            while (cur)
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+
+            // 栈里面取到左路节点，它左路节点左子树访问完了
+            TreeNode* top = st.top();
+            st.pop();
+            v.push_back(top->val);
+
+            //访问左路节点右子树  --子问题
+            cur = top->right;
+        }
+        return v;
+    }
+};
