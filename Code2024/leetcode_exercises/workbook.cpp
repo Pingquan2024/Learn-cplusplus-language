05bdc3dc9f8c80fedda7035767b0ac9ca6c82c5b
Pingquan2024 <esrgfu@gmail.com>
2024-05-07 21:49:06 +0800

    最小花费爬楼梯

--- Code2024/leetcode_exercises/workbook.cpp
+++ Code2024/leetcode_exercises/workbook.cpp
@@ -296,3 +296,22 @@ public:
         return v;
     }
 };
+
+//746 使用最小花费爬楼梯
+class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int>& cost)
+    {
+        int n = cost.size();
+        vector<int> dp(n + 1);
+        dp[1] = dp[0] = 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[n];
+    }
+};
