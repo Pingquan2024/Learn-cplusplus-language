25a5444c4f61d7e07c121ed29b25abebb875dfae
Pingquan2024 <esrgfu@gmail.com>
2024-05-17 23:34:25 +0800

    c++11

--- Code2024/c++11/test1.cpp
+++ Code2024/c++11/test1.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
-int main()
-{
 
+int main() 
+{
+	int i = 10; 
+	auto p = &i;
+	auto pf = strcpy;
+	cout << typeid(p).name() << endl; 
+	cout << typeid(pf).name() << endl;
+	map<string, string> dict = { {"sort", "≈≈–Ú"}, {"insert", "≤Â»Î"} }; 
+	//map<string, string>::iterator it = dict.begin(); auto it = dict.begin();
 	return 0;
 }
