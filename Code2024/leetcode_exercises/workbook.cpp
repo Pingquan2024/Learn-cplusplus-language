080f7dc21634ebc7555cbd232aa2a2b3bc2e9ebd
Pingquan2024 <esrgfu@gmail.com>
2024-06-20 23:59:28 +0800

    leetcode

--- Code2024/leetcode_exercises/workbook.cpp
+++ Code2024/leetcode_exercises/workbook.cpp
@@ -489,15 +489,19 @@ public:
     }
 };
 
-class Solution {
+class Solution 
+{
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(vector<int>& prices) 
+    {
         int n = (int)prices.size(), ans = 0;
         for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
+            for (int j = i + 1; j < n; ++j) 
+            {
                 ans = max(ans, prices[j] - prices[i]);
             }
         }
         return ans;
     }
 };
+
