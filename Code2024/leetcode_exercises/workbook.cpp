940ce7bfbfe857fa0aa34ee00bdc6f6634a884c8
Pingquan2024 <esrgfu@gmail.com>
2024-06-21 23:49:03 +0800

    twoSum

--- Code2024/leetcode_exercises/workbook.cpp
+++ Code2024/leetcode_exercises/workbook.cpp
@@ -505,3 +505,15 @@ public:
     }
 };
 
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+
+        }
+    }
+};
