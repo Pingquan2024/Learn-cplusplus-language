477ba5009333f55b1c4cbdca90f6a4725e4a0c9e
Pingquan2024 <esrgfu@gmail.com>
2024-06-28 23:52:46 +0800

    auto_ptr

--- Code2024/SmartPtr/test.cpp
+++ Code2024/SmartPtr/test.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+
 using namespace std;
 
+// 智能指针是管理动态分配的内存的，它会帮助我们自动释放new出来的内存，从而避免内存泄漏！
+// RAII
 namespace fpq
 {
 
@@ -12,9 +15,7 @@ namespace fpq
 	public:
 		auto_ptr(T* ptr)
 			:_ptr(ptr)
-		{
-			
-		}
+		{ }
 	
 		~auto_ptr()
 		{
@@ -57,8 +58,34 @@ void test_auto()
 	*ap2 = 1;
 }
 
+class Test
+{
+public:
+	Test()
+	{
+		cout << "Test构造函数" << endl;
+	}
+
+	~Test()
+	{
+		cout << "Test析构函数" << endl;
+	}
+
+	int getDebug()
+	{
+		return this->debug;
+	}
+
+private:
+	int debug = 22;
+};
+
 int main()
 {
 	test_auto();
+
+	auto_ptr<Test> t1(new Test);		// auto_ptr 定义智能指针
+	cout << "Test->debug" << t1->getDebug() << endl;
+	cout << "(*test).debug" << (*t1).getDebug() << endl;
 	return 0;
 }
