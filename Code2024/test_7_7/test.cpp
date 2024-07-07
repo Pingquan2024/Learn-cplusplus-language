b4f4c88646f4833dbe4ad02f43a94abf6adbc71b
Pingquan2024 <esrgfu@gmail.com>
2024-07-07 23:54:37 +0800

    2024年7月7日代码

---
+++ Code2024/test_7_7/test.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int L, R;
+//    int count = 0;
+//    cin >> L >> R;
+//
+//    while (L <= R)
+//    {
+//        int tmp = L;
+//        while (tmp != 0)
+//        {
+//            if (tmp % 10 == 2)
+//            {
+//                tmp /= 10;
+//                count++;
+//            }
+//            else
+//            {
+//                tmp /= 10;
+//            }
+//        }
+//
+//        L++;
+//    }
+//
+//    cout << count << endl;
+//
+//    return 0;
+//}
+
