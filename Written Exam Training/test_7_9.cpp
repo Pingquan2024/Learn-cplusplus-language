dcee0a19e625289b9e8bba5e92e56445aca9aab7
Pingquan2024 <esrgfu@gmail.com>
2024-07-10 23:31:24 +0800

    简写单词 dd爱框框 /2

--- Written Exam Training/test_7_9.cpp
+++ Written Exam Training/test_7_9.cpp
@@ -1,3 +1,27 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//string s;
+//
+//int main()
+//{
+//    while (cin >> s)
+//    {
+//        if (s[0] >= 'a' && s[0] <= 'z')
+//        {
+//            cout << (char)(s[0] - 32);
+//        }
+//        else
+//        {
+//            cout << s[0];
+//        }
+//    }
+//
+//    return 0;
+//}
+
 #include <iostream>
 
 using namespace  std;
@@ -30,11 +54,51 @@ int main()
                 retRight = right;
                 retLen = right - left + 1;
             }
-            sum -= arr[left];
+            sum -= arr[left++];
         }
         right++;
     }
 
-    cout << left << " " << right << endl;
+    cout << retLeft << " " << retRight << endl;
 
 }
+
+
+//#include <iostream>
+//#include <queue>
+//
+//using namespace std;
+//
+//int n, k, x;
+//long long sum;
+//priority_queue<long long> heap;
+//
+//int main()
+//{
+//    cin >> n >> k;
+//    while (n--)
+//    {
+//        cin >> x;
+//        sum += x;
+//        if (x % 2 == 0)
+//        {
+//            heap.push(x);
+//        }
+//    }
+//
+//    // 这样heap里的就都是偶数
+//    while (heap.size() && k--)
+//    {
+//        long long t = heap.top();
+//        heap.pop();
+//        sum -= t / 2;
+//        if ((t / 2) % 2 == 0)
+//        {
+//            heap.push(t / 2);
+//        }
+//    }
+//
+//    cout << sum << endl;
+//
+//    return 0;
+//}
