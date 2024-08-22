0c1d074ea837eba5f328982e446aa497871b6c6d
Pingquan2024 <esrgfu@gmail.com>
2024-08-22 23:09:59 +0800

    平方数、分组

--- Written Exam Training/test_7_23.cpp
+++ Written Exam Training/test_7_23.cpp
@@ -1,4 +1,4 @@
-//
+/* 平方数 */
 //#include <iostream>
 //
 //using namespace std;
@@ -22,7 +22,27 @@
 //    return 0;
 //}
 
+/* 平方数 */
+//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//
+//typedef long long LL;
+//
+//LL x;
+//
+//int main()
+//{
+//    cin >> x;
+//    LL a = sqrt(x);
+//    LL y1 = a * a, y2 = (a + 1) * (a + 1);
+//    if (x - y1 < y2 - x) cout << y1 << endl;
+//    else cout << y2 << endl;
+//}
 
+
+/* 分组 */
 //#include <algorithm>
 //#include <iostream>
 //
@@ -86,3 +106,56 @@
 //
 //    return 0;
 //}
+
+
+/* 分组 */
+//#include <iostream>
+//#include <unordered_map>
+//
+//using namespace std;
+//
+//int n, m;
+//unordered_map<int, int> myMap;
+//
+//bool check(int i)
+//{
+//    int g = 0; // 能分成多少组
+//    for (auto& [a, b] : myMap)
+//    {
+//        g += b / i + (b % i == 0 ? 0 : 1);
+//    }
+//
+//    return g <= m;
+//}
+//
+//int main()
+//{
+//    cin >> n >> m;
+//    int hmax = 0; // 代表声部最多的人数
+//    for (int i = 0; i < n; i++)
+//    {
+//        int x = 0;
+//        cin >> x;
+//        hmax = max(hmax, ++myMap[x]);
+//    }
+//
+//    int sz = myMap.size();
+//    if (sz > m)
+//    {
+//        cout << -1 << endl;
+//    }
+//    else
+//    {
+//        // 枚举最后的结果中，最多的人数
+//        for (int i = 1; i < hmax; i++)
+//        {
+//            if (check(i))
+//            {
+//                cout << i << endl;
+//                break;
+//            }
+//        }
+//    }
+//    return 0;
+//}
+
