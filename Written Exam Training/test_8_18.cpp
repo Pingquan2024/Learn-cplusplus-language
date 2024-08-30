4e2f7fa0c23adb5e0f80f0d6ae43bf505f799c30
Pingquan2024 <esrgfu@gmail.com>
2024-08-30 23:28:15 +0800

    2024/8/18

---
+++ Written Exam Training/test_8_18.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int n;
+//vector<int> ret;
+//
+//int main()
+//{
+//	cin >> n;
+//	while (n > 0)
+//	{
+//		// 1. ÕÒ³ö¸öÎ»
+//		int tmp = n % 10;
+//		if(tmp == 0)
+//	}
+//	return 0;
+//}
